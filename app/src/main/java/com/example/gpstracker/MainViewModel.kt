b27837475c97e362f5cb0a4debdea83da2ce9c8f
andrew <andart777@gmail.com>
2025-02-09 15:09:10 +0600

    Add constructor in MainViewModel

--- app/src/main/java/com/example/gpstracker/MainViewModel.kt
+++ app/src/main/java/com/example/gpstracker/MainViewModel.kt
@@ -2,11 +2,36 @@ package com.example.gpstracker
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.gpstracker.db.MainDb
+import com.example.gpstracker.db.TrackItem
 import com.example.gpstracker.location.LocationModel
+import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+@Suppress("UNCHECKED_CAST")
+class MainViewModel(db: MainDb) : ViewModel() {
+    val dao = db.getDao()
     val locationUpdates = MutableLiveData<LocationModel>()
+
     // Для безопасной передачи данных в textview, в случае если он еще не нарисован или fragment изменен (переворот экрана) будет ошибка
     // (в MutableLiveData добавляется специальный обсервер который следить за циклом жизни нашего фрагмента)
     var timeData = MutableLiveData<String>()
+
+    val tracks = dao.getAllTracks().asLiveData()
+
+    fun insertTrack(trackItem: TrackItem) = viewModelScope.launch {
+        dao.insertTrack(trackItem)
+    }
+
+    // Этот класс необходим, что бы MainViewModel был конструктор (db: MainDb) для передачи в него базы данных.
+    class ViewModelFactory(private val db: MainDb) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                return MainViewModel(db) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
